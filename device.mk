10443132d448a9aa2eb2a5f4382b13761bc0bfa8
daniml3 <daniel@danielml.dev>
2022-03-26 09:56:46 -0300

    spes: Shim the GetPreviewImageData symbol on libpiex
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I321c28e0021bc7f7e37d9c98398f2c8a469a4a13

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
