f4fe661825b588a78899cc64580ff46925c5b87c
boedhack99 <boedhack@gmail.com>
2022-07-16 09:01:50 +0700

    spes: Komodo-OS
    Change-Id: I8b699fc6ef17e38b5f94e308fe0678ed1aa04d8d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/komodo_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spes-user \
-    lineage_spes-userdebug \
-    lineage_spes-eng
+    komodo_spes-user \
+    komodo_spes-userdebug \
+    komodo_spes-eng
--- komodo_spes.mk
+++ komodo_spes.mk
@@ -8,19 +8,33 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Komodo-OS stuff.
+$(call inherit-product, vendor/komodo/config/common.mk)
 
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-PRODUCT_NAME := lineage_spes
+# Komodo Stuff
+KOMODO_OFFICIAL := true
+KOMODO_GAPPS_TYPE := gapps
+KOMODO_VARIANT := RELEASE
+
+PRODUCT_NAME := komodo_spes
 PRODUCT_DEVICE := spes
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Some Build Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_USES_AOSP_CHARGER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="spes-user 11 RKQ1.211001.001 V13.0.5.0.RGCMIXM release-keys"
